5de2db9843a2a2a7ae3f164888af86413da2fc0a
pflarue <131492283+pflarue@users.noreply.github.com>
2024-05-31 06:48:22 -0700

    Append '+develop' to version in develop branch.

--- ARDOPCommonCode/version.h
+++ ARDOPCommonCode/version.h
@@ -16,4 +16,4 @@
 // ardopcf was a mistake.  This was due to a bug and misunderstanding
 // of the ardop version numbering scheme.  So ardopcf v1.0.4.1.1 comes 
 // after and replaces v2.0.3.2.1
-const char ProductVersion[] = "1.0.4.1.2";
+const char ProductVersion[] = "1.0.4.1.2+develop";
